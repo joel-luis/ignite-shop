c2b73dd90fa880c899c76907fef5f931ef5ca148
Joel Luis <joelluis73@gmail.com>
2022-11-10 21:01:51 -0300

    create success page ssr

---
+++ src/layouts/SuccessLayout/index.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { SuccessProps } from 'pages/success'
+import * as S from './styles'
+
+export function SuccesLayout({ customerName, product }: SuccessProps) {
+  return (
+    <S.SuccessContainer>
+      <h1>Compra efetuada!</h1>
+      <S.ImageContainer>
+        <Image src={product.imageUrl} width={120} height={110} alt="" />
+      </S.ImageContainer>
+      <p>
+        Uhuul <strong>{customerName}</strong>, sua{' '}
+        <strong>{product.name}</strong> já está a caminho da sua casa.
+      </p>
+      <Link href="/">Voltar ao catálogo</Link>
+    </S.SuccessContainer>
+  )
+}
